223f7a3684818e058e3b0302e42dd4e02e35c328
ThiagoServulo <thiagoservulog@gmail.com>
2023-09-14 19:45:15 -0300

    Updating configuration.py

--- configuration.py
+++ configuration.py
@@ -1,2 +1,10 @@
-CAMERA_IP = "192.168.0.154"
+# Camera typo of connection
+#   0 = webcam
+#   1 = remote camera (using IP to connect)
+CAMERA = 1
+
+# Camera connection IP
+CAMERA_IP = "192.168.0.2"
+
+# Camera connection port
 CAMERA_PORT = "4747"
