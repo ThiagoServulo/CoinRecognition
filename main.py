b30d267b5d028a20947ce4ef8514f8b0178b365b
ThiagoServulo <thiagoservulog@gmail.com>
2023-09-27 21:00:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ while True:
     else:
         raise TypeError("Select the type of the camera")
 
-    img_result =  image.copy()
-
     # Pre-processing the image
     imagePreProcessed = PreProcessing.ImagePreProcessing(image.copy())
     #cv2.imshow('Image Pre-processed', imagePreProcessed)
@@ -65,36 +63,65 @@ while True:
             image_cutout = cv2.GaussianBlur(image_cutout, (3, 3), 3)
             #cv2.imshow('Image cutout', image_cutout)
 
-            ##############
-
-            # Resize the raw image into (224-height,224-width) pixels
+            # Resize the image
+            # Parameters:
+            #   - (224, 244): New image size in pixels
+            #   - cv2.INTER_AREA: The average of the pixels is calculated within an original pixel area
             image_cutout = cv2.resize(image_cutout, (224, 224), interpolation=cv2.INTER_AREA)
             cv2.imshow('Image cutout resized', image_cutout)
 
-            # Make the image a numpy array and reshape it to the models input shape.
+            # Make the image a numpy array and reshape it to the models input shape
+            # Parameters:
+            #   - 1: indicates that we are dealing with a single sample
+            #   - 224 indicates the width of the image after resizing
+            #   - 224 indicates the height of the image after resizing
+            #   - 3 indicates that the image has 3 color channels (RGB)
             image_cutout = np.asarray(image_cutout, dtype=np.float32).reshape(1, 224, 224, 3)
 
             # Normalize the image array
+            # Dividing by the maximum possible value (127.5 in this case)
+            # Next, the 1 is subtracted from each pixel value. This adjusts the values to the desired range of -1 to 1
             image_cutout = (image_cutout / 127.5) - 1
 
-            # Predicts the model
+            # Make predictions using the model created
             prediction = model.predict(image_cutout)
+
+            # Find the index of the maximum value in the prediction array
             index = np.argmax(prediction)
+
+            # Determine the predicted class label associated with the input data
             class_name = class_names[index]
+
+            # Get the score of the confidence recognizing
             confidence_score = prediction[0][index]
 
-            # Print prediction and confidence score
-            classe = class_name[2:]
+            # Get the class name (The name of the coing recognized)
+            coin_name = class_name[2:-1]
 
-            conf = str(np.round(confidence_score * 100))[:-2]
+            # Calculate the confidence score in percentage
+            confidence_score = str(np.round(confidence_score * 100))[:-2]
 
-            if int(conf) > 50:
-                print("Class:", classe, end="")
-                print("Confidence Score:", conf, "%")
+            # Check the score of the confidence
+            if int(confidence_score) > 50:
+                print("Coin name:", coin_name)
+                print("Confidence Score:", confidence_score, "%")
 
-                img_result = cv2.putText(img_result, f'{classe}: {conf}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                # Write the text with the coin information in the image
+                # Parameters:
+                #   - (x, y): These are the coordinates where the text will be written
+                #   - cv2.FONT_HERSHEY_SIMPLEX: The font type to be used for the text
+                #   - 0.5: Defines the font scale factor, which determines the size of the text relative to the image
+                #   - (255, 255, 255): The color of the text in the format (B, G, R)
+                #   - 2: Specifies the thickness of the text
+                image = cv2.putText(image.copy(), f'{coin_name}: {confidence_score}', (x, y),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            else:
+                print(f"Coin desconsidered because your confidence score is too low")
+                #print("Coin name:", coin_name)
+                #print("Confidence Score:", confidence_score, "%")
 
-        cv2.imshow("Resultado", img_result)
+        # Show the final image, with the coins recognized
+        cv2.imshow("Coins recognized", image)
 
     # Listen to the keyboard for presses
     keyboard_input = cv2.waitKey(1)
